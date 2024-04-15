2d17df8d65f926f850a58102d6f5cdc4f865b4f7
szymongrabski <grabskiszymonom@gmail.com>
2024-04-15 12:31:00 +0200

    Added UploadModal component

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 
 const Library = () => {
+    const authModal = useAuthModal();
+    const uploadModal = useUploadModal();
+    const { user } = useUser();
+
     const onClick = () => {
-        // handle upload
+        if (!user) {
+            return authModal.onOpen();
+        }
+        // check for subscription 
+        return uploadModal.onOpen();
     }
     return (
         <div className="flex flex-col">

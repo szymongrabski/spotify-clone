077703e4e4c9711e5cd00b63b5c963616066edeb
szymongrabski <grabskiszymonom@gmail.com>
2024-04-15 22:04:10 +0200

    Search page and song fetching

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,10 +3,16 @@
 import useAuthModal from "@/hooks/useAuthModal";
 import useUploadModal from "@/hooks/useUploadModal";
 import { useUser } from "@/hooks/useUser";
+import { Song } from "@/types";
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+    songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user } = useUser();
@@ -34,7 +40,13 @@ const Library = () => {
                 />
             </div>
             <div className="flex flex-col gap-y-2 mt-4 px-3">
-                    List of songs
+                    {songs.map((item) => (
+                        <MediaItem 
+                            onClick={() => {}}
+                            key={item.id}
+                            data={item}
+                        />
+                    ))}
             </div>
         </div>
     )

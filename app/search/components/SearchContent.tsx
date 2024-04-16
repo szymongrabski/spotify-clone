6e4d97619e8b52f1260d1ca38349b21a1e753641
szymongrabski <grabskiszymonom@gmail.com>
2024-04-16 17:53:28 +0200

    Added like functionality

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types"
 
@@ -36,6 +37,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
               data={song}
             />
           </div>
+          <LikeButton songId={song.id}/>
         </div>
       ))}
     </div>
